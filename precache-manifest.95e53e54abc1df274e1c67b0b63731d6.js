ea5f32294fa476dcbfa33653746ed5dcddfb8c36
dedsec <arefmaddah@gmail.com>
2019-10-06 11:09:25 +0330

    Updates

---
+++ precache-manifest.95e53e54abc1df274e1c67b0b63731d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ee48b8ea34319ba679a91175853b35",
+    "url": "/my_resume/index.html"
+  },
+  {
+    "revision": "8411a9afe4e1280ef5cb",
+    "url": "/my_resume/static/css/main.2666b1ec.chunk.css"
+  },
+  {
+    "revision": "4a070baf34de674444c3",
+    "url": "/my_resume/static/js/2.9ccd9347.chunk.js"
+  },
+  {
+    "revision": "8411a9afe4e1280ef5cb",
+    "url": "/my_resume/static/js/main.b73f1822.chunk.js"
+  },
+  {
+    "revision": "894501bb81b12ba5e80c",
+    "url": "/my_resume/static/js/runtime-main.3a5f27a5.js"
+  }
+]);
